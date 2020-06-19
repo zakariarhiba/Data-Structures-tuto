2604668b82d4c75630aa0898d3ea7c548102ed33
zakaria <zakariarhiba@gmail.com>
2020-06-19 16:00:46 +0200

    add tuple

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# tuple kichbah lbzzf list wlkn houya mat9drch tbdl fih 
+
+# tuple syntax 
+tup = (0,1,3,4)
+tup1 = ('zaka','hello','welcome')
+
+print(tup + tup1)
+
+# kifma glna mat9drch tbdl index 
+''' error tup[2] = 4 '''
+
+# ga3 les applications d list t9dr dirhom f tuple ila tghyr value
